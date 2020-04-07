a19b2320dd57a724f32a571bf3d3cdfca2b9d2f1
Montana <montana@Montanas-MacBook-Pro.local>
2020-04-07 11:52:42 -0400

    Adding dictionary course file

---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,27 @@
+/*
+ Montana Braswell
+ April 7th, 2020
+ Dev Launchpad Course
+ */
+
+import UIKit
+
+/*
+  A dictionary is a collection of items, that have labels called key value pairs
+  A dictionary also has constant time look up
+ */
+
+
+let devices: [String: String] = [
+ 
+    "phone" : "iPhone X",
+    "laptop": "2016 Macbook Pro",
+    "tablet": "2018 iPad Pro",
+    "headphones": "Bose Quiet Comfort 35 Wireless Headphones II"
+]
+
+
+// To access values in a dictionary, we need to look it up with a key.
+
+devices["laptop"]
+devices["phone"]
