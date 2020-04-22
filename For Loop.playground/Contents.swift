6435681f775ff29ce4052ccf19185df78dd72ef3
Montana <montana@Montanas-MacBook-Pro.local>
2020-04-21 20:49:37 -0400

    Uploading For Loop file

---
+++ For Loop.playground/Contents.swift
@@ -0,0 +1,38 @@
+  
+/* Montana Braswell
+   April 20th,2020
+   Dev Launchpad Course
+ */
+
+import UIKit
+
+    
+// a for loop is used for iterate over a collection like an array, set or an dictionary
+// Also to do something a number of times
+  
+  
+
+// This is a collection we can iterate over
+  
+  let allStars = ["James","Davis","Harden","Doncic","Leonard"]
+
+// for every player in the allStars array, I want to print their name
+  for players in allStars {
+  print(players)
+  }
+
+  
+  var randomInts: [Int] = []
+  
+  
+  
+  
+  
+// running a for loop 25 times
+  for _ in 0..<25 {
+    let randomNumber = Int.random(in: 0...100)
+    randomInts.append(randomNumber)
+  }
+
+
+  print(randomInts)
